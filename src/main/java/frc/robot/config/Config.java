4b0bc61b5a9102b3ef30ce56dc0ae80c86ab3935
Matthew Pomes <the10thwiz@gmail.com>
2018-10-16 21:52:31 -0700

    First commit
    Add base classes and the drivetrain / tankdrive, along with a simple
    config tool. Basic Teleop, and no autonomous.

---
+++ src/main/java/frc/robot/config/Config.java
@@ -0,0 +1,119 @@
+package frc.robot.config;
+
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.List;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import org.json.simple.parser.JSONParser;
+
+@SuppressWarnings("unchecked")
+public class Config {
+	private final String name;
+	private final Map<String, Object> configOBJ;
+	
+	public Config(String name) {
+		this.name = name;
+		try {
+			configOBJ = (Map<String, Object>) new JSONParser()
+			        .parse(new InputStreamReader(Config.class.getResourceAsStream(name+".json")));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Config Failed to parse");
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Object get(String path) {
+		Object o = get(path, configOBJ);
+		if (o == null) {
+			throw new RuntimeException("Path '" + path + "' does not exist in config " + name);
+		}
+		return o;
+	}
+	
+	private Object get(String path, Object config) {
+		if (path.length() == 0) {
+			return config;
+		} else if (config instanceof Map) {
+			String part = path.substring(0, Math.min(path.indexOf('.'), path.indexOf('[')));
+			return get(path.substring(Math.min(path.indexOf('.'), path.indexOf('[')) + 1),
+			        ((Map<String, Object>) config).get(part));
+		} else if (config instanceof List) {
+			String part = path.substring(0, path.indexOf(']'));
+			return get(path.substring(path.indexOf(']') + 2), ((List<Object>) config).get(Integer.parseInt(part)));
+		} else {
+			return null;
+		}
+	}
+	
+	public int getInt(String path) {
+		Object n = get(path);
+		if (n instanceof Number) {
+			return ((Number) n).intValue();
+		} else {
+			throw new RuntimeException("Path '" + path + "' is not an int in config " + name);
+		}
+	}
+	
+	public double getDouble(String path) {
+		Object n = get(path);
+		if (n instanceof Number) {
+			return ((Number) n).doubleValue();
+		} else {
+			throw new RuntimeException("Path '" + path + "' is not a double in config " + name);
+		}
+	}
+	
+	public boolean getBool(String path) {
+		Object n = get(path);
+		if (n instanceof Boolean) {
+			return ((Boolean) n).booleanValue();
+		} else {
+			throw new RuntimeException("Path '" + path + "' is not a boolean in config " + name);
+		}
+	}
+	
+	public List<Object> getList(String path) {
+		Object n = get(path);
+		if (n instanceof List) {
+			return (List<Object>) n;
+		} else {
+			throw new RuntimeException("Path '" + path + "' is not a list in config " + name);
+		}
+	}
+	
+	public Map<String, Object> getMap(String path) {
+		Object n = get(path);
+		if (n instanceof Map) {
+			return (Map<String, Object>) n;
+		} else {
+			throw new RuntimeException("Path '" + path + "' is not a map in config " + name);
+		}
+	}
+	
+	public TalonSRX createTalon(String path) {
+		TalonSRX motor = new TalonSRX(getInt(path + ".id"));
+		try {
+			motor.setInverted(getBool(path + ".reversed"));
+		} catch (Exception e) {
+		}
+		
+		return motor;
+	}
+	
+	public VictorSPX createVictor(String path) {
+		VictorSPX motor = new VictorSPX(getInt(path + ".id"));
+		try {
+			motor.setInverted(getBool(path + ".reversed"));
+		} catch (Exception e) {
+		}
+		
+		return motor;
+	}
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.config.Config;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrain extends Subsystem {
+	
+	public static final String PATH = "drivetrain";
+	private final TalonSRX leftMaster;
+	private final TalonSRX left2;
+	private final TalonSRX left3;
+	private final TalonSRX rightMaster;
+	private final TalonSRX right2;
+	private final TalonSRX right3;
+	
+	public DriveTrain(Config config) {
+		leftMaster = config.createTalon(PATH + ".left1");
+		left2 = config.createTalon(PATH + ".left2");
+		left3 = config.createTalon(PATH + ".left3");
+		rightMaster = config.createTalon(PATH + ".right1");
+		right2 = config.createTalon(PATH + ".right2");
+		right3 = config.createTalon(PATH + ".right3");
+
+		left2.follow(leftMaster);
+		left3.follow(leftMaster);
+
+		right2.follow(rightMaster);
+		right3.follow(rightMaster);
+	}
+	
+	/**
+	 * Sets the speed values as percentages 
+	 * @param left Left demand, [-1, 1]
+	 * @param right Right demand, [-1, 1]
+	 */
+	public void set(double left, double right) {
+		leftMaster.set(ControlMode.PercentOutput, left);
+		rightMaster.set(ControlMode.PercentOutput, right);
+	}
+
+	/**
+	 * Sets the speed to 0
+	 */
+	public void stop() {
+		leftMaster.set(ControlMode.PercentOutput, 0);
+	}
+
+	@Override
+	public void initDefaultCommand() {
+	}
+}
